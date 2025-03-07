23277db24160f9545b084352b980dc3fa480c821
pgao1116 <pgao000@citymail.cuny.edu>
2025-03-07 01:58:04 -0500

    Connected backend to cloud database

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
-dotenv.config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Required for Render PostgreSQL
+  }
 });
 
 module.exports = pool;
